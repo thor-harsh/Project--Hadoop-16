e04d3a8baa69628cfab66719e5ad312c7209093d
thor-harsh <105957576+thor-harsh@users.noreply.github.com>
2023-10-29 23:22:17 +0530

    Add files via upload

---
+++ word-count-better.py
@@ -0,0 +1,20 @@
+import re
+from pyspark import SparkConf, SparkContext
+
+#Use regular expression to strip out the words out of all punctuations and
+#then tells that it may contain unicode information and then splitted it
+#into different words and converted it into lowercase words
+def normalizeWords(text):
+    return re.compile(r'\W+', re.UNICODE).split(text.lower())
+
+conf = SparkConf().setMaster("local").setAppName("WordCount")
+sc = SparkContext(conf = conf)
+
+input = sc.textFile("Book")
+words = input.flatMap(normalizeWords)
+wordCounts = words.countByValue()
+
+for word, count in wordCounts.items():
+    cleanWord = word.encode('ascii', 'ignore')
+    if (cleanWord):
+        print(cleanWord.decode() + " " + str(count))
